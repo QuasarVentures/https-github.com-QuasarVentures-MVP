aed134e9f52db7df39dc0324e0b942417fd020d6
Roberto Carlos García Luis <rgarcia@kairos.systems>
2019-02-08 17:17:30 -0600

    WIP: Contract translation to spanish

--- src/i18n/es/index.js
+++ src/i18n/es/index.js
@@ -2,7 +2,6 @@ export default {
   langLabel: 'es',
   langName: 'Español',
   welcome: 'Hola',
-  sendMessage: 'Messager',
   gigs: {
     createEdit: {
       form: {
@@ -25,9 +24,11 @@ export default {
       update: 'Actualizar'
     },
     industry: {
-      '3724': '3724 - Electricians',
-      '7380': '7380 - Trucking',
-      '5190': '5190 - Electricians'
+      '3724': '3724 - Electricistas',
+      '7380': '7380 - Camioneros',
+      '5190': '5190 - Electricistas',
+      'NBA': 'NBA',
+      'NFL': 'NFL'
     },
     list: {
       new: 'Nuevo Trabajo',
@@ -50,6 +51,7 @@ export default {
   layout: {
     menu: {
       gigs: 'trabajos',
+      profile: 'Perfil',
       stats: 'Estadísticas',
       users: 'Usuarios'
     }
@@ -63,6 +65,19 @@ export default {
       }
     },
     registerLink: '¿Aún no tienes cuenta? Registrate.'
+  },
+  profile: {
+    downloadContract: 'Descargar mi contrato',
+    maritalStatus: {
+      M: 'Casado',
+      S: 'Soltero',
+      undefined: 'Privado'
+    },
+    sex: {
+      F: 'Mujer',
+      M: 'Hombre'
+    },
+    update: 'Actualizar'
   },
   register: {
     registerForm: {
@@ -126,5 +141,40 @@ export default {
       required: 'Este campo es requerido',
       terms: 'Debes estar de acuerdo con los términos y servicio para continuar'
     }
+  },
+  contract: {
+    independent: 'CONTRATISTA INDEPENDIENTE',
+    driverEnrollment: '\nFORMULARIO DE INSCRIPCIÓN PARA EL CONDUCTOR \nPARA ACCIDENTE OCUPACIONAL \nCOBERTURA',
+    insuredPerson: 'Información de la persona asegurada:',
+    completeName: 'Apellido\t\tNombre\tInicial',
+    address: 'Dirección',
+    completeLoc: 'Ciudad\t\tEstado\t\tCódigo Postal',
+    phone: 'Teléfono: ',
+    ssn: '# Seguro Social: ',
+    birthday: 'Fecha de Nacimiento: ',
+    sex: 'Sexo: ',
+    F: 'M',
+    M: 'H',
+    marital: 'Estado civil: ',
+    beneficiary: 'Información del beneficiario:',
+    orPay: 'o pagar a mis herederos',
+    relationship: 'Parentesco',
+    effectiveDate: 'Fecha efectiva de contrato: ',
+    desiredDate: 'Fecha efectiva de seguro deseado: ',
+    cn: 'Número de contrato: ',
+    leasedTo: 'Arrendado a: ',
+    locationNumber: 'Número: ',
+    quasarInfo: 'Quasar Risk Advisors\nP.O. Box\nLand O Lakes, FL 34639\nOficina - 813-540-6833\nFax - 813-333-2966\ngeneralemailbox@quasar here\n',
+    acceptance: 'Aprobación',
+    acceptanceText: 'También reconozco que no he buscado ni recibido asesoramiento de seguros de QUASAR sobre la cobertura de seguro a la que se hace referencia, ya que se aplica a mí y / o a mis necesidades de negocios. También entiendo y acepto que QUASAR RISK \n ADVISORS utiliza un administrador de reclamos externo que está autorizado \n para llevar a cabo los deberes razonables y habituales de un \'administrador de reclamos para la cobertura de seguro que aquí se ofrece. Además, entiendo y acepto que la compensación por los servicios prestados por lo anterior es parte del costo de la cobertura del seguro y no me corresponde por separado. \n \n De acuerdo con mi contrato de arrendamiento y como un anexo al mismo, \nI, El Contratista Independiente, autoriza a la compañía patrocinadora a \ndividir periódicamente los costos de mi seguro. Si tales acuerdos (u otros \nmonies debidos a usted) no son suficientes para permitir la deducción del costo, Remitiré mediante cheque certificado o giro postal el costo pendiente de seguro a Quasar Risk Advisors, PO Box "TBD", Land O \nLakes, FL 34639 dentro de un período de diez (10) días. De lo contrario, no comprendo que los aseguradores de seguros pueden cancelar esta cobertura de seguro dentro de los términos y condiciones de la póliza. También entiendo que \nla cobertura no se restablecerá automáticamente si la cancelación es \nprocess. \n ',
+    acceptanceSign: 'Firma:\n\nFecha:',
+    pageOne: 'página 1 de 2',
+    form: 'ICOA - Formulario de inscripción',
+    terms: 'Términos y condiciones',
+    read: 'Por favor lea cuidadosamente',
+    understood: 'Se entiende y acuerda:',
+    clausesOne: '1. Compensación a los trabajadores no obligatoria y Responsabilidad de los empleadores: esta cobertura NO es una póliza de Compensación y responsabilidad civil de los Trabajadores y los beneficios de esta póliza no son necesariamente iguales a los beneficios a los que una persona no puede acceder. n Compensación a los trabajadores. Sin embargo, la persona asegurada está de acuerdo en que, en caso de un accidente laboral, buscará en este programa en lugar de buscar trabajadores.’ Compensación \ncoverage. \n \n2. Asignación de Beneficios: En el caso de que los beneficios se paguen por \nLa política de Compensación al Trabajador y Responsabilidad del Empleador \n La política de la compañía patrocinadora o cualquier póliza similar se haga responsable de tales beneficios, los beneficios a los que una \n persona Asegurada tiene derecho bajo este La política se asignará de forma nautomática a la empresa patrocinadora. Por lo tanto, en este caso, la Persona Asegurada acuerda la asignación inmediata a la compañía patrocinadora de todos los beneficios que reciba o de lo contrario sería pagadero a él o a cualquier tercero bajo este programa. \n \n3. Costo y consentimiento para el cambio de costo: la persona asegurada entiende que el costo del seguro que se muestra puede incluir impuestos, gastos de administración y gastos administrativos que acepta y reconoce como parte del costo del seguro. Los aseguradores \n no aseguradores se reservan el derecho de cambiar la tarifa enviándole un aviso por escrito. \n \n4. Evidencia de Cobertura: Su estado de operaciones principales \npuede requerir que compre Trabajadores Estatutarios \nCompensación; de lo contrario, la compañía patrocinadora se encargará de este Programa de Accidente Ocupacional del Contratista Independiente. Si no participa en este \n Programa de Accidentes Ocupacionales, debe tener en archivo un \ncertificado de seguro de Compensación Legal por Accidentes de Trabajo \ni cuando esté permitido por la ley, un Certificado de Accidente Ocupacional; su agente de seguros debe proporcionar esta información a la \n compañía patrocinadora. \n \nUsted estará inscrita en el Programa de contratistas independientes \n hasta que presente pruebas de la cobertura adecuada. En ese momento \n, recibirá un ajuste en cualquier costo que se le cobre. \n ',
+    clausesTwo: '5. Rescisión: en el caso de que el contrato de arrendamiento del contratista con la compañía patrocinadora sea cancelado por alguna de las partes, la cobertura de Accidente Ocupacional se cancelará a partir de la fecha de rescisión del contrato o la fecha más próxima que la ley permita. Debe hacer \narrangements para reemplazar la cobertura inmediatamente. \n \n6. Términos y condiciones: La cobertura estará sujeta a todos los términos, condiciones y exclusiones de la póliza que se detallan en la Evidencia de seguro de \n. La persona asegurada debe ser menor de 75 años para inscribirse en el plan. \n \n7. Autorización de la Deducción de Liquidación: Usted, el \nContratista Independiente, autoriza a la empresa patrocinadora a tomar de sus cheques de liquidación, fondos, acumulaciones u otra compensación de \n, de forma periódica (por ejemplo, semanal o mensualmente) en cantidades suficientes para pagar el costo de primas e impuestos y, por lo tanto, solicítele que lo reenvíe directamente a Quasar Risk Advisors, por ejemplo, a más tardar el 15 de cada mes. Su costo (como se describe en "Costo y consentimiento para calificar") se deducirá de su cheque de liquidación. Si su cheque de liquidación no es suficiente para cubrir el costo del seguro para usted, se le pedirá que envíe un cheque o un giro postal a nombre de Quasar Risk Advisors a la dirección que se indica a continuación, o la cobertura será cancelado, de acuerdo con los términos y condiciones de la política. \n \n8. Fecha de entrada en vigencia: la cobertura entrará en vigencia a partir de la \nde la fecha en que Midlands \nGestion, haya aceptado su solicitud, pero no antes del primer día del \n mes después de que complete y firme este formulario de inscripción.\n',
+    pageTwo: 'página 2 de 2'
   }
 }
